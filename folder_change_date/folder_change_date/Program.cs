3b4b58c0113ca61bedfe1d412f45732c611fd2c7
Maxim Zhuchkovsky <55227329+MaximZhuchkovsky730501@users.noreply.github.com>
2023-01-31 19:50:14 +0300

    Add files via upload

---
+++ folder_change_date/folder_change_date/Program.cs
@@ -0,0 +1,21 @@
+﻿
+namespace folder_change_date
+{
+    class Programm
+    {
+        static void Main(string[] args)
+        {
+            string folder = "\\\\localhost\\temp folder";
+            int minutes = 1;
+            while (true)
+            {
+                DateTime change_time = Directory.GetLastWriteTime(folder);
+                DateTime now_time = DateTime.Now;
+                if (change_time.AddMinutes(minutes) < now_time)
+                {
+                    Console.WriteLine(change_time.ToString());
+                }
+            }
+        }
+    }
+}
